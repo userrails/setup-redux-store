8dd7abd6c30e85e26320293b965db93c936f43b9
Shiv Raj <shivrajbadu@gmail.com>
2019-06-24 11:07:22 +0545

    code refactoring

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import './App.css';
 import Routes from './routes';
+import NavbarHeader from './app/NavbarHeader';
+import {BrowserRouter} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <div>
-        <Routes />
+    <BrowserRouter>
+      <div className="App">
+        <div>
+          <NavbarHeader />
+        </div>
+        <header className="App-header">
+          <p>Welcome</p>
+        <div>
+          <Routes />
+        </div>
+        </header>
       </div>
-      <header className="App-header">
-        <p>Welcome</p>
-      </header>
-    </div>
+    </BrowserRouter>
   );
 }
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom';
-import NavbarHeader from '../app/NavbarHeader';
+import {Route, Switch, Redirect} from 'react-router-dom';
 
 import Home from '.././pages/Home';
 import Contact from '.././pages/Contact';
@@ -8,18 +7,12 @@ import About from '.././pages/About';
 
 export default () => {
   return (
-    <BrowserRouter>
       <div>
-        <div>
-          <NavbarHeader />
-        </div>
-
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/contact" component={Contact} />
           <Route path="/about" component={About} />
         </Switch>
       </div>
-    </BrowserRouter>
   )
 }
