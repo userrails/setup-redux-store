1aa519574159cb14a8f13efb8601c87fdeeefe86
Shiv Raj <shivrajbadu@gmail.com>
2019-06-24 15:49:31 +0545

    added deps

--- src/App.js
+++ src/App.js
@@ -2,23 +2,21 @@ import React from 'react';
 import './App.css';
 import Routes from './routes';
 import NavbarHeader from './app/NavbarHeader';
-import {BrowserRouter} from 'react-router-dom';
+
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <div>
-          <NavbarHeader />
-        </div>
-        <header className="App-header">
-          <p>Welcome</p>
-        <div>
-          <Routes />
-        </div>
-        </header>
+    <div className="App">
+      <div>
+        <NavbarHeader />
       </div>
-    </BrowserRouter>
+      <header className="App-header">
+        <p>Welcome</p>
+      <div>
+        <Routes />
+      </div>
+      </header>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,8 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router';
+import configureStore, { history } from './configureStore';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore(/* provide initial state if any */)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}> { /* place ConnectedRouter under Provider */}
+      <> { /* your usual react-router v4/v5 routing */}
+        <App history={history}/>
+      </>
+    </ConnectedRouter>
+  </Provider>,
+ document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
